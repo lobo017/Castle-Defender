e17e8e0da148dbde10beb0e8229bc656a1220176
lobo017 <edaniel.lobo@gmail.com>
2025-11-17 16:36:32 -0600

    post video project

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance { get; private set; }
+
+    public LevelData[] allLevels;
+    public LevelData CurrentLevel { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        CurrentLevel = allLevels[0];
+    }
+    public void LoadLevel(LevelData levelData)
+    {
+        CurrentLevel = levelData;
+        SceneManager.LoadScene(levelData.levelName);
+    }
+}
